7d171982c7ba01defee0e830d3c06ee2236a6023
Dmitrijs Milajevs <dimazest@gmail.com>
2013-11-06 10:25:21 +0000

    Migration to github, travis and coveralls

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 
 version = '0.1'
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = 'test'
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 
 setup(
@@ -37,4 +53,7 @@ setup(
             'corpora = fowler.corpora.main:dispatch',
         ],
     },
+    tests_require=['pytest>=2.4.2'],
+    cmdclass={'test': PyTest},
+
 )
