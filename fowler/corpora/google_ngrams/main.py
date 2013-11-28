272100c6cbb2b8e0471e70287c28a831dd67cc17
Dmitrijs Milajevs <dimazest@gmail.com>
2013-11-28 11:19:48 +0000

    Google ngram cooccurrence.

--- fowler/corpora/google_ngrams/main.py
+++ fowler/corpora/google_ngrams/main.py
@@ -4,16 +4,15 @@
 Version 20120701 dataset.
 
 """
-import sys
-import csv
+import json
 from collections import Counter
 from itertools import product
+
 
 from opster import Dispatcher
 from py.path import local
 
-from fowler.corpora.io import readline_folder
-from .util import readline_google_store, iter_google_store, Record
+from google_ngram_downloader import readline_google_store
 
 
 dispatcher = Dispatcher()
@@ -21,39 +20,24 @@ command = dispatcher.command
 
 
 @command()
-def download(
-    ngram_len=('n', 1, 'The length of ngrams to be downloaded.'),
-    output=('o', 'downloads/google_ngrams/{ngram_len}', 'The destination folder for downloaded files.'),
-    verbose=('v', False, 'Be verbose.'),
-):
-    """Download The Google Books Ngram Viewer dataset version 20120701."""
-    output = local(output.format(ngram_len=ngram_len))
-    output.ensure_dir()
-
-    for fname, url, request in iter_google_store(ngram_len, verbose=verbose):
-        with output.join(fname).open('wb') as f:
-
-            for num, chunk in enumerate(request.iter_content(1024)):
-                    if verbose and not divmod(num, 1024)[1]:
-                        sys.stderr.write('.')
-                        sys.stderr.flush()
-                    f.write(chunk)
-
-
-@command()
 def cooccurrence(
     ngram_len=('n', 2, 'The length of ngrams to be downloaded.'),
-    output=('o', 'downloads/google_ngrams/{ngram_len}_coocurrence_matrix', 'The destination folder for downloaded files.'),
+    output=('o', 'downloads/google_ngrams/{ngram_len}_cooccurrence_matrix/', 'The destination folder for downloaded files.'),
     verbose=('v', False, 'Be verbose.'),
 ):
     assert ngram_len > 1
-
     middle_index = ngram_len // 2
+    output_dir = local(output.format(ngram_len=ngram_len))
 
-    for fname, url, records in readline_google_store(ngram_len, verbose=verbose):
+    for fname, _, records in readline_google_store(ngram_len, verbose=verbose):
+        output_file = output_dir.join(fname + '.json')
+
+        if output_file.check():
+            continue
+
         cooc = Counter()
 
-        for record in records:
+        for i, record in enumerate(records, start=1):
 
             ngram = record.ngram.split()
             # Filter out any annotations. E.g. removes `_NUM` from  `+32_NUM`
@@ -65,22 +49,8 @@ def cooccurrence(
 
             cooc.update({p: count for p in product([item], context)})
 
-        print(ngram)
-        print(cooc)
-
-        break
+        with output_file.open('w') as f:
+            json.dump(cooc, f)
 
 
-@command()
-def count_ngrams(path=None):
-    """Count total number of ngrams in the collection."""
-    with readline_folder(path) as data:
 
-        reader = csv.reader(
-            data,
-            delimiter='\t',
-        )
-
-        records = map(Record._make, reader)
-        ngrams = set(r.ngram for r in records)
-        print(len(ngrams))
