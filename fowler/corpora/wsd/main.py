76b1e0ae41fa19f9219f8ed55d4c0173d85f493c
Dmitrijs Milajevs <dimazest@gmail.com>
2014-05-16 19:24:05 +0100

    Paraphrasing of short sentences.

--- fowler/corpora/wsd/main.py
+++ fowler/corpora/wsd/main.py
@@ -37,6 +37,33 @@ class WSDDispatcher(Dispatcher, SpaceMixin):
 
         return grouped
 
+    @Resource
+    def ks13_data(self):
+        """
+        Paraphrasing dataset provided by [1] and re-scored by [2].
+
+        [1] Mitchell, J., & Lapata, M. (2010). Composition in distributional
+            models of semantics. Cognitive science, 34(8), 1388-1429.
+
+        [2] Kartsaklis, D., & Sadrzadeh, M. Prior Disambiguation of Word Tensors
+            for Constructing Sentence Vectors.
+
+        """
+        index_cols = 'subject1', 'verb1', 'object1', 'subject2', 'verb2', 'object2'
+
+        data = pd.read_csv(
+            self.kwargs['ks13_data'],
+            sep=' ',
+            usecols=index_cols + ('score', ),
+        )
+
+        grouped = data.groupby(index_cols, as_index=False).mean()
+
+        if self.limit:
+            grouped = grouped.head(self.limit)
+
+        return grouped
+
 
 dispatcher = WSDDispatcher()
 command = dispatcher.command
@@ -75,7 +102,67 @@ def gs11(
     [2] http://www.cs.ox.ac.uk/activities/compdistmeaning/GS2011data.txt
 
     """
+    similarity_experiment(
+        space,
+        pool,
+        gs11_data,
+        verb_columns=('verb', 'landmark'),
+        similarity_input=lambda verb_vectors, S: (
+            (
+                (v, verb_vectors[v]),
+                (s, space[S(s)]),
+                (o, space[S(o)]),
+                (l, verb_vectors[l]),
+            )
+            for v, s, o, l in gs11_data[['verb', 'subject', 'object', 'landmark']].values
+        ),
+        similarity_function=gs11_similarity,
+        input_column='input'
+    )
 
+    display(gs11_data.groupby('hilo').mean())
+
+
+@command()
+def paraphrasing(
+    pool,
+    space,
+    ks13_data=('', 'downloads/compdistmeaning/emnlp2013_turk.txt', 'The KS2013 dataset.'),
+):
+    similarity_experiment(
+        space,
+        pool,
+        ks13_data,
+        verb_columns=('verb1', 'verb2'),
+        similarity_input=lambda verb_vectors, S: (
+            (
+                (s1, space[S(s1)]),
+                (v1, verb_vectors[v1]),
+                (o1, space[S(o1)]),
+                (s2, space[S(s2)]),
+                (v2, verb_vectors[v2]),
+                (o2, space[S(o2)]),
+            )
+            for s1, v1, o1, s2, v2, o2 in ks13_data[['subject1', 'verb1', 'object1', 'subject2', 'verb2', 'object2']].values
+        ),
+        similarity_function=paraphrasing_similarity,
+        input_column='score'
+    )
+
+
+def paraphrasing_similarity(args):
+    (s1, subject1), (v1, verb1), (o1, object1), (s2, subject2), (v2, verb2), (o2, object2) = args
+
+    def Sentence(subject, verb, object_):
+        return verb.multiply(compose(subject, object_))
+
+    s1 = Sentence(subject1, verb1, object1)
+    s2 = Sentence(subject2, verb2, object2)
+
+    return pairwise.cosine_similarity(s1, s2)[0][0]
+
+
+def similarity_experiment(space, pool, data, verb_columns, similarity_input, similarity_function, input_column):
     def T(w, tag):
         if space.row_labels.index.nlevels == 2:
             return w, tag
@@ -85,7 +172,7 @@ def gs11(
     V = lambda v: T(v, 'VERB')
     S = lambda s: T(s, 'SUBST')
 
-    verbs = pd.concat([gs11_data['verb'], gs11_data['landmark']]).unique()
+    verbs = pd.concat([data[vc] for vc in verb_columns]).unique()
     verb_vectors = Bar(
         'Verb vectors',
         max=len(verbs),
@@ -96,30 +183,21 @@ def gs11(
     verb_vectors = dict(verb_vectors)
 
     similarities = pool.imap(
-        gs11_similarity,
-        (
-            (
-                (v, verb_vectors[v]),
-                (s, space[S(s)]),
-                (o, space[S(o)]),
-                (l, verb_vectors[l]),
-            )
-            for v, s, o, l in gs11_data[['verb', 'subject', 'object', 'landmark']].values
-        ),
+        similarity_function,
+        similarity_input(verb_vectors, S)
     )
 
-    gs11_data['Cosine similarity'] = list(
+    data['Cosine similarity'] = list(
         Bar(
             'Cosine similarity',
-            max=(len(gs11_data)),
+            max=(len(data)),
             suffix='%(index)d/%(max)d, elapsed: %(elapsed_td)s',
         ).iter(similarities)
     )
 
-    display(gs11_data.groupby('hilo').mean())
     sns.jointplot(
-        gs11_data['input'],
-        gs11_data['Cosine similarity'],
+        data[input_column],
+        data['Cosine similarity'],
         kind='reg',
         stat_func=stats.spearmanr,
         xlim=(1, 7),
@@ -128,5 +206,7 @@ def gs11(
 
     print(
         'Spearman correlation: rho={0:.2f}, p={1:.2f}'
-        .format(*stats.spearmanr(gs11_data[['input', 'Cosine similarity']]))
+        .format(*stats.spearmanr(data[[input_column, 'Cosine similarity']]))
     )
+
+    return data
