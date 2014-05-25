89d53a6d2af977df0f3bf457a8e7ff90dfdf50b6
Dmitrijs Milajevs <dimazest@gmail.com>
2014-05-25 23:18:33 +0100

    Partial transitive_verb_space() implementation.

--- fowler/corpora/categorical/main.py
+++ fowler/corpora/categorical/main.py
@@ -1,10 +1,15 @@
 """Categorical vector space creation."""
+import logging
 
-import numpy as np
 import pandas as pd
+
+from progress.bar import Bar
+from scipy import sparse
 
 from fowler.corpora.dispatcher import Dispatcher, Resource, NewSpaceCreationMixin, SpaceMixin, DictionaryMixin
 from fowler.corpora.space.util import read_tokens
+
+logger = logging.getLogger(__name__)
 
 
 class CategoricalDispatcher(Dispatcher, NewSpaceCreationMixin, SpaceMixin, DictionaryMixin):
@@ -24,7 +29,11 @@ class CategoricalDispatcher(Dispatcher, NewSpaceCreationMixin, SpaceMixin, Dicti
                 self.kwargs['transitive_verb_arguments'],
                 key=self.dictionary_key,
             )
-            [['verb_stem', 'subj_stem', 'obj_stem', 'count']]
+            [
+                ['verb_stem', 'subj_stem', 'subj_tag', 'obj_stem', 'obj_tag', 'count']
+            ]
+            # Because we get rid of the verb, there might be multiple entries for a verb_stem!
+            .groupby(('verb_stem', 'subj_stem', 'subj_tag', 'obj_stem', 'obj_tag'), as_index=False).sum()
             .set_index('verb_stem')
             .loc[self.verbs]
             .reset_index()
@@ -32,19 +41,26 @@ class CategoricalDispatcher(Dispatcher, NewSpaceCreationMixin, SpaceMixin, Dicti
 
     @Resource
     def subjects(self):
-        return self.transitive_verb_arguments['subj_stem'].unique()
+        return (
+            self
+            .transitive_verb_arguments[['subj_stem', 'subj_tag']]
+            .drop_duplicates()
+            .rename(columns={'subj_stem': 'ngram', 'subj_tag': 'tag'})
+        )
 
     @Resource
     def objects(self):
-        return self.transitive_verb_arguments['obj_stem'].unique()
+        return (
+            self
+            .transitive_verb_arguments[['obj_stem', 'obj_tag']]
+            .drop_duplicates()
+            .rename(columns={'obj_stem': 'ngram', 'obj_tag': 'tag'})
+        )
 
     @Resource
     def subjects_objects(self):
-        return pd.DataFrame(
-            {
-                'ngram': np.unique(np.concatenate((self.subjects, self.objects))),
-            }
-        )
+        return pd.concat((self.subjects, self.objects)).drop_duplicates()
+
 
 dispatcher = CategoricalDispatcher()
 command = dispatcher.command
@@ -55,23 +71,60 @@ def extract_verb_arguments(
     subjects_objects,
     output=('o', 'targets.csv', 'File where targets should be written.'),
     lowercase=('', False, 'Lowercase tokens.'),
-    pos_tag=('', 'SUBST', 'The part of speech tag.')
 ):
     if lowercase:
         subjects_objects['ngram'] = subjects_objects['ngram'].str.lower()
         subjects_objects.drop_duplicates(inplace=True)
 
-    if pos_tag:
-        subjects_objects['tag'] = pos_tag
-
     subjects_objects.to_csv(output, index=False)
 
 
 @command()
-def transitive_verbs_space(
+def transitive_verb_space(
     space,
-    subjects,
-    objects,
+    transitive_verb_arguments,
+    dictionary_key,
+    pool,
+    output=('o', 'space.h5', 'Output verb vector space.'),
+    chunk_size=('', 10000, 'The length of the chunk.'),
 ):
+    groups = transitive_verb_arguments.groupby(
+        ['subj_stem', 'subj_tag', 'obj_stem', 'obj_tag'],
+    )
+
+    # groups = Bar(
+    #     'Subject object Kronecker products',
+    #     max=1,
+    #     # max=len(transitive_verb_arguments[['subj_stem', 'subj_tag', 'obj_stem', 'obj_tag']]),
+    #     suffix='%(index)d/%(max)d, elapsed: %(elapsed_td)s',
+    # ).iter(groups)
+
+    verb_tensors = {}
+
+    for (subj_stem, subj_tag, obj_stem, obj_tag), group in groups:
+        # There have to be at most one identical subject, object tuple per verb.
+        assert len(group['verb_stem'].unique()) == len(group)
+
+        subject_vector = space[subj_stem, subj_tag]
+        object_vector = space[obj_stem, obj_tag]
+
+        if not subject_vector.size:
+            logger.warning('Subject %s %s is empty!', subj_stem, subj_tag)
+            continue
+
+        if not object_vector.size:
+            logger.warning('Object %s %s is empty!', obj_stem, obj_tag)
+            continue
+
+        subject_object_tensor = sparse.kron(subject_vector, object_vector)
+
+        for verb_stem, count in group[['verb_stem', 'count']].values:
+
+            t = subject_object_tensor * count
+
+            if verb_stem not in verb_tensors:
+                verb_tensors[verb_stem] = t
+            else:
+                verb_tensors[verb_stem] = verb_tensors[verb_stem] + t
 
     import ipdb; ipdb.set_trace()
