9d32b05580c86d2da62854b741d085be55147159
Dmitrijs Milajevs <dimazest@gmail.com>
2014-12-11 15:23:25 +0000

    Worker class

--- fowler/corpora/util.py
+++ fowler/corpora/util.py
@@ -1,4 +1,5 @@
 from docutils.examples import html_body
+from progress.bar import Bar
 
 
 def inside_ipython():
@@ -22,3 +23,20 @@ def display(value):
 def rst_to_html(value):
     return html_body(value)
 
+
+class Worker:
+    def __init__(self, pool, show_progress_bar=False):
+        self.show_progress_bar = show_progress_bar
+        self.pool = pool
+
+    def progressify(self, iterable, description, max=None):
+        if self.show_progress_bar:
+            return Bar(
+                description,
+                max=max,
+                suffix='%(index)d/%(max)d, elapsed: %(elapsed_td)s',
+            ).iter(
+                iterable
+            )
+        else:
+            return iterable
