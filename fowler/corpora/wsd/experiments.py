5a70950fc71e716e49c9a1bdb0589a02d4c51d51
Dmitrijs Milajevs <dimazest@gmail.com>
2015-12-10 15:18:14 +0000

    Experiments

--- fowler/corpora/wsd/experiments.py
+++ fowler/corpora/wsd/experiments.py
@@ -1,16 +1,31 @@
 import logging
+from itertools import chain
 
 import pandas as pd
 
 from colored import style
 from scipy import stats
 from scipy.spatial import distance
-from sklearn.metrics import pairwise
 
 from fowler.corpora.util import Worker
 
 
 logger = logging.getLogger(__name__)
+
+
+def _tree(dg, i):
+    node = dg.get_by_address(i)
+
+    deps = sorted(chain.from_iterable(node['deps'].values()))
+
+    return node['lemma'], node['tag'], tuple(_tree(dg, dep) for dep in deps)
+
+
+def tree(dg):
+        node = dg.root
+
+        deps = sorted(chain.from_iterable(node['deps'].values()))
+        return node['lemma'], node['tag'], tuple(_tree(dg, dep) for dep in deps)
 
 
 class SimilarityExperiment(Worker):
@@ -33,12 +48,12 @@ class SimilarityExperiment(Worker):
         result = pd.DataFrame.from_records(
             [
                 (
-                    str(s1.tree()),
-                    str(s2.tree()),
+                    tree(s1),
+                    tree(s2),
 
                     1 / (1 + distance.euclidean(s1_vect, s2_vect)),
-                    -distance.cosine(s1_vect, s2_vect) + 1,
-                    -distance.correlation(s1_vect, s2_vect) + 1,
+                    1 - distance.cosine(s1_vect, s2_vect),
+                    1 - distance.correlation(s1_vect, s2_vect),
                     s1_vect.dot(s2_vect.T),
 
                     score,
@@ -61,6 +76,9 @@ class SimilarityExperiment(Worker):
                 'score',
             ) + getattr(dataset, 'extra_fields', tuple()),
         )
+
+        if not result.notnull().all().all():
+            logger.warning('Null values in similarity scores.')
 
         rho, p = stats.spearmanr(result[['cos', 'score']])
         print(
