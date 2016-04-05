c975dedc7aac1e68dbc4fe90642bfe2c52b4565f
Dmitrijs Milajevs <dimazest@gmail.com>
2016-04-05 16:46:10 +0100

    Updating the dataset

--- test/test_andailment.py
+++ test/test_andailment.py
@@ -11,29 +11,18 @@ def dataset(ANDailment_path):
 def test_read_file(dataset):
     df = dataset.read_file()
 
-    import pdb; pdb.set_trace()
+    assert len(df) == 462
 
 
-    # train = df[df['split'] == 'train']
-    # assert len(train) == 4076
-    #
-    # test = df[df['split'] == 'test']
-    # assert len(test) == 1725
+def test_dependency_graphs_pairs(dataset):
+    g1, g2, score = next(dataset.dependency_graphs_pairs())
+
+    assert score == 0
 
 
-# def test_dependency_graphs_pairs(dataset):
-#     g1, g2, score = next(dataset.dependency_graphs_pairs())
-#
-#     assert score == 1
-#
-#
-# def test_words_by_document(dataset):
-#     words, = dataset.words_by_document(dataset.paths[0])
-#
-#     assert next(words) == ('PCCW', 'PCCW', 'N')
-#     assert next(words) == ("'s", "'s", 'P')
-#     assert next(words) == ('chief', 'chief', 'N')
-#     assert next(words) == ('operating', 'operate', 'V')
-#     assert next(words) == ('officer', 'officer', 'N')
-#
-#     list(words)
+def test_words_by_document(dataset):
+    words, = dataset.words_by_document(dataset.paths[0])
+
+    assert next(words) == ('entrepreneur', 'entrepreneur', 'N')
+
+    assert list(words)
